c580a1a8d9930e915759917d2e632ac7c8ae7590
Ananthu S Kuttattu <anandhus.kuttattu@gmail.com>
2019-02-25 11:39:54 +0530

    Add files via upload
    implemented counter function

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -1,5 +1,6 @@
 const Todo = require('../model/todo.model');
 var auth = require('basic-auth')
+var seq;
 
 exports.todoAuth = function(req, res) {
     var credentials = auth(req);
@@ -14,7 +15,7 @@ exports.todoAuth = function(req, res) {
 
 }
 exports.todo_all = function(req, res) {
-    Todo.find(function(err, todos) {
+    Todo.Todolist.find(function(err, todos) {
         if (err) {
             return next(err);
         }
@@ -22,38 +23,88 @@ exports.todo_all = function(req, res) {
     })
 }
 exports.todo_create = function(req, res) {
-    var todo = new Todo({
-        title: req.body.title,
-        description: req.body.description,
-        targetTime: req.body.targetTime,
-        priority: req.body.priority,
-        status: req.body.status
 
+    var todo;
+    Todo.Counter.count(function(err, count) {
+        console.log("count");
+        if (!err && count === 0) {
+            var myobj = new Todo.Counter({ Cid: "todo_id", sequence_value: 0 });
+            console.log(myobj);
+            myobj.save(function(err) {
+                if (err) {
+                    console.log(myobj);
+                } else
+                    getCounter();
+            });
+
+        } else
+            getCounter();
     });
-    todo.save(function(err) {
-        if (err) {
-            return next(err);
+
+    function addtodo(val) {
+        this.seq = val;
+
+        todo = new Todo.Todolist();
+        todo.taskId = this.seq;
+        todo.title = req.body.title;
+        todo.description = req.body.description;
+        todo.targetTime = req.body.targetTime;
+        todo.priority = req.body.priority;
+        todo.status = req.body.status;
+
+
+        todo.save(function(err) {
+
+            if (err) {
+                console.log(err);
+            }
+            res.send('Todo Created successfully  ' + todo.taskId)
+        })
+
+    }
+
+    function getCounter() {
+
+        Todo.Counter.findOneAndUpdate({ Cid: "todo_id" }, { $inc: { sequence_value: 1 } }, function(error, counter) {
+            if (error)
+                console.log("Can't find and update todo_id");
+            else
+                findtodoidandcreatetodo();
+        });
+
+
+        function findtodoidandcreatetodo() {
+            Todo.Counter.findOne({ Cid: "todo_id" }, function(err, counter) {
+                if (err)
+                    console.log("Can't find 'todo_id' ");
+                console.log(counter.sequence_value);
+                addtodo(counter.sequence_value);
+            });
         }
-        res.send('Todo Created successfully  ' + todo.title)
-    })
+
+    }
+
 };
 
 exports.todo_details = function(req, res) {
-    Todo.findById(req.params.id, function(err, todo) {
-        if (err) return next(err);
+
+    Todo.Todolist.findOne({ taskId: req.params.id }, function(err, todo) {
+        if (err) console.log(err);
         res.send(todo);
-    })
+    });
 };
 
 exports.todo_update = function(req, res) {
-    Todo.findByIdAndUpdate(req.params.id, { $set: req.body }, function(err, todo) {
-        if (err) return next(err);
+
+    Todo.Todolist.findOneAndUpdate({ taskId: req.params.id }, { $set: req.body }, function(error, counter) {
+        if (error)
+            console.log("Can't find and update todo_id");
         res.send('todo udpated.');
     });
 };
 
 exports.todo_delete = function(req, res) {
-    Todo.findByIdAndRemove(req.params.id, function(err) {
+    Todo.Todolist.findOneAndRemove({ taskId: req.params.id }, function(err) {
         if (err) return next(err);
         res.send('Todo deleted successfully!');
     })
